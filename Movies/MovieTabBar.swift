fd9292b091197955613d430c3faf2de16d236e9d
Kevin Yan <kevinyan215@gmail.com>
2020-08-08 22:47:29 -0700

    Movies Tab bar

---
+++ Movies/MovieTabBar.swift
@@ -0,0 +1,144 @@
+//
+//  MovieTabBar.swift
+//  Movies
+//
+//  Created by Kevin Yan on 8/8/20.
+//  Copyright © 2020 Kevin Yan. All rights reserved.
+//
+
+import UIKit
+
+class MovieTabBar: UIView {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+    var tabSelections = ["Popular", "Now Playing", "Latest", "Top Rated", "Upcoming"]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        collectionView.register(MenuCell.self, forCellWithReuseIdentifier: "MenuCell")
+        setupView()
+        backgroundColor = UIColor.gray
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        self.addSubview(collectionView)
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+//        NSLayoutConstraint.activate([
+//            collectionView.topAnchor.constraint(equalTo: self.topAnchor),
+//            collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+//            collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+//            collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+//        ])
+        
+        addConstraintsWithFormat("H:|[v0]|", views: collectionView)
+        addConstraintsWithFormat("V:|[v0]|", views: collectionView)
+    }
+}
+
+extension MovieTabBar : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tabSelections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath) as? MenuCell {
+            cell.backgroundColor = UIColor.blue
+            cell.label.text = tabSelections[indexPath.row]
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    
+}
+
+extension MovieTabBar: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width / 5, height: frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+ 
+class MenuCell: BaseCell {
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.tintColor = UIColor.rgb(red: 91, green: 14, blue: 13)
+        return label
+    }()
+    
+    override var isHighlighted: Bool {
+        didSet {
+            label.tintColor = isHighlighted ? UIColor.white : UIColor.rgb(red: 91, green: 14, blue: 13)
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            label.tintColor = isSelected ? UIColor.white : UIColor.rgb(red: 91, green: 14, blue: 13)
+        }
+    }
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(label)
+        addConstraintsWithFormat("H:[v0(28)]", views: label)
+        addConstraintsWithFormat("V:[v0(28)]", views: label)
+        
+        addConstraint(NSLayoutConstraint(item: label, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: label, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+    }
+    
+}
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    func setupViews() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView {
+    func addConstraintsWithFormat(_ format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
