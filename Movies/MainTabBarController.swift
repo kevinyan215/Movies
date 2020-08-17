13e3074a517813905f637d9422296771eb0c3887
Kevin Yan <kevinyan215@gmail.com>
2020-08-16 21:57:55 -0700

    Added icons to each tab bar

--- Movies/MainTabBarController.swift
+++ Movies/MainTabBarController.swift
@@ -17,12 +17,15 @@ class MainTabBarController : UITabBarController {
     func setupTabBar() {
         let movieCollectionVC = UINavigationController(rootViewController: MovieCollectionViewController())
         movieCollectionVC.tabBarItem.title = "Movies"
+        movieCollectionVC.tabBarItem.image = UIImage(named: "movies_icon")
         
         let myMoviesVC = UINavigationController(rootViewController: MyMoviesViewController())
         myMoviesVC.tabBarItem.title = "My Movies"
+        myMoviesVC.tabBarItem.image = UIImage(named: "my_movies_icon")
         
         let searchVC = UINavigationController(rootViewController: SearchViewController())
         searchVC.tabBarItem.title = "Search"
+        searchVC.tabBarItem.image = UIImage(named: "search_icon")
         
         var accountVC: UIViewController
         if userIsSignedIn() {
@@ -31,9 +34,9 @@ class MainTabBarController : UITabBarController {
 
         } else {
             accountVC = UINavigationController(rootViewController: SignInViewController())
-
         }
         accountVC.tabBarItem.title = "Account"
+        accountVC.tabBarItem.image = UIImage(named: "account_icon")
         
         viewControllers = [movieCollectionVC, myMoviesVC, searchVC, accountVC]
     }
